da483c0b57d5f6e0ed74468d61b0f768dc4dce5d
Sadman Arnob <sadmanarnob31@gmail.com>
2023-05-10 23:26:02 +0600

    comment added

--- database.php
+++ database.php
@@ -8,7 +8,6 @@ $con = "";
 
 try{
     $con = mysqli_connect($dbMechine, $userName, $pass, $dbName);
-    echo "database is connected";
 }catch(mysqli_sql_exception)
 {
     echo "database is not connected";
